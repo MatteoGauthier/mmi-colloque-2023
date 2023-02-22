7f99ffbcc1c5b3acf6dc25425dfaaa9b72393c55
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-02-22 14:56:07 +0100

    [ENHANCEMENT] Structure of the info page (with tw config colors + fonts) (#5)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -11,6 +13,10 @@ module.exports = {
         inclusivity: "#FFE279",
         public: "#F370E6",
         device: "#70E3F3",
+      },
+      fontFamily: {
+        display: ["Hind", ...defaultTheme.fontFamily.sans],
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
       },
     },
   },
