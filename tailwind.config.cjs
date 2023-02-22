4ba6f9ec3c872bca46da6404c5761d4bdc3a4e5e
Maxime <lust.maxime@outlook.fr>
2023-02-22 12:49:15 +0100

    Feat: tailwind config colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,9 @@ module.exports = {
       },
       colors: {
         accessibility: "#FC8833",
+        inclusivity: "#FFE279",
+        public: "#F370E6",
+        device: "#70E3F3",
       },
     },
   },
