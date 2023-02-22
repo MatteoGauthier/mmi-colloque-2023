191803095f1ef771c5a5e095c66e1c6d2c2db90c
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-02-22 12:05:41 +0100

    [TAILWIND] Setup TailwindCSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontSize: {
+        title: "3.125rem",
+      },
+      colors: {
+        accessibility: "#FC8833",
+      },
+    },
+  },
+  plugins: [],
 }
