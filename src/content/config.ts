f0e2698a524b3285b5528fb5c48a1fcb900dfed2
ccadran <clariocadran33@gmail.com>
2023-02-22 15:27:43 +0100

    structure publications + add 2 topics

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,31 +1,42 @@
-import { defineCollection, z } from "astro:content"
+import { defineCollection, z } from "astro:content";
 
 const topic = defineCollection({
-  schema: z.object({
-    name: z.string(),
-    description: z.string(),
-
-    speakers: z.array(
-      z.object({
+    schema: z.object({
         name: z.string(),
-      })
-    ),
+        description: z.string(),
 
-    live: z.object({
-      url: z.string(),
-      title: z.string(),
+        speakers: z.array(
+            z.object({
+                name: z.string(),
+            })
+        ),
+
+        live: z.object({
+            url: z.string(),
+            title: z.string(),
+        }),
+        poster: z.object({
+            title: z.string(),
+            firstContent: z.string(),
+            img: z.string(),
+        }),
+        publication: z.object({
+            title: z.string(),
+            firstContent: z.string(),
+            secondContent: z.string(),
+            thirdContent: z.string(),
+        }),
+
+        information: z.object({
+            title: z.string(),
+            firstContent: z.string(),
+            stats: z.object({
+                value: z.number(),
+                label: z.string(),
+            }),
+            secondContent: z.string(),
+        }),
     }),
+});
 
-    information: z.object({
-      title: z.string(),
-      firstContent: z.string(),
-      stats: z.object({
-        value: z.number(),
-        label: z.string(),
-      }),
-      secondContent: z.string(),
-    }),
-  }),
-})
-
-export const collections = { topic }
+export const collections = { topic };
